ae65e7ce722b651ddcffdb787acd776070676518
Aman-K-Ray <aman.kumar.a.ray@accenture.com>
2026-03-24 13:39:33 +0530

    removed footer

--- src/App.jsx
+++ src/App.jsx
@@ -94,7 +94,7 @@ export const App = () => {
             </main>
           </AppWrapper>
         </ActiveTabProvider>
-        <FooterSlot />
+        {/* <FooterSlot /> */}
       </div>
     </>
   );
