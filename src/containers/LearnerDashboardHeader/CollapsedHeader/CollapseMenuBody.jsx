b64210dabe2a4c6fd038991b18855199b80c0b7e
Aman-K-Ray <aman.kumar.a.ray@accenture.com>
2026-07-22 15:13:40 +0530

    header fix

--- src/containers/LearnerDashboardHeader/CollapsedHeader/CollapseMenuBody.jsx
+++ src/containers/LearnerDashboardHeader/CollapsedHeader/CollapseMenuBody.jsx
@@ -4,57 +4,25 @@ import PropTypes from 'prop-types';
 import { getConfig } from '@edx/frontend-platform';
 import { useIntl } from '@edx/frontend-platform/i18n';
 import { AppContext } from '@edx/frontend-platform/react';
-import { Button, Badge } from 'ts-paragon';
-import { useMasqueradeBarData } from '../../MasqueradeBar/hooks';
+import { Button } from 'ts-paragon';
 
 import WidgetNavbar from 'containers/WidgetContainers/WidgetNavbar';
-import urls from 'data/services/lms/urls';
 import { reduxHooks } from 'hooks';
 import { COLLAPSED_NAVBAR } from 'widgets/RecommendationsPaintedDoorBtn/constants';
 
-import { findCoursesNavDropdownClicked } from '../hooks';
+
 import messages from '../messages';
-import { useActiveTab } from '../../../ActiveTabContext';
 
 export const CollapseMenuBody = ({ isOpen }) => {
   const { formatMessage } = useIntl();
-  const siteNameMessage = formatMessage(messages['with.site.name'], { siteName: getConfig().SITE_NAME });
-  
+
   const { authenticatedUser } = React.useContext(AppContext);
-  const {
-    canMasquerade,
-  } = useMasqueradeBarData({ authenticatedUser });
 
   const dashboard = reduxHooks.useEnterpriseDashboardData();
-  const { courseSearchUrl } = reduxHooks.usePlatformSettingsData();
-
-  const exploreCoursesClick = findCoursesNavDropdownClicked(urls.baseAppUrl(courseSearchUrl));
-
-  // Get groupedCourses and tabNames
-  const groupedCourses = reduxHooks.useGroupedCoursesData();
-  const tabNames = reduxHooks.useOrderedCoursesLabel() || [];
-  const { activeTab } = useActiveTab();
-
-  // Get the current tab's courseProvider name
-  let courseDisName = '';
-  if (groupedCourses && tabNames.length > 0) {
-    const tabKey = tabNames[activeTab];
-    const tabObj = groupedCourses && groupedCourses[tabKey];
-    const tabCourses = tabObj && Array.isArray(tabObj.courses) ? tabObj.courses.flat() : [];
-    if (tabCourses.length > 0 && tabCourses[0].courseProvider) {
-      courseDisName = tabCourses[0].course.courseName;
-    }
-  }
 
   return (
     isOpen && (
       <div className="d-flex flex-column shadow-sm nav-small-menu">
-        {courseDisName && (
-          <Button as="a" variant="inverse-primary" className="courseOrgDisName">
-              {courseDisName}
-            {courseDisName.includes("National AI Olympiad") && "™"}
-          </Button>
-        )}
         
         <WidgetNavbar placement={COLLAPSED_NAVBAR} />
         {authenticatedUser && (
--- src/containers/LearnerDashboardHeader/CollapsedHeader/index.jsx
+++ src/containers/LearnerDashboardHeader/CollapsedHeader/index.jsx
@@ -10,11 +10,29 @@ import CollapseMenuBody from "./CollapseMenuBody";
 import BrandLogo from "../BrandLogo";
 
 import messages from "../messages";
+import { useActiveTab } from '../../../ActiveTabContext';
+import { reduxHooks } from 'hooks';
 
 export const CollapsedHeader = () => {
   const { formatMessage } = useIntl();
   const isCollapsed = useIsCollapsed();
   const { isOpen, toggleIsOpen } = useLearnerDashboardHeaderData();
+
+  // Get groupedCourses and tabNames
+  const groupedCourses = reduxHooks.useGroupedCoursesData();
+  const tabNames = reduxHooks.useOrderedCoursesLabel() || [];
+  const { activeTab } = useActiveTab();
+
+  // Get the current tab's courseProvider name
+  let courseDisName = '';
+  if (groupedCourses && tabNames.length > 0) {
+    const tabKey = tabNames[activeTab];
+    const tabObj = groupedCourses && groupedCourses[tabKey];
+    const tabCourses = tabObj && Array.isArray(tabObj.courses) ? tabObj.courses.flat() : [];
+    if (tabCourses.length > 0 && tabCourses[0].courseProvider) {
+      courseDisName = tabCourses[0].course.courseName;
+    }
+  }
 
   return (
     isCollapsed && (
@@ -35,6 +53,16 @@ export const CollapsedHeader = () => {
             className="p-4"
           />
           <BrandLogo />
+          {courseDisName && (
+              <div className="courseOrgDisName">
+                {courseDisName && (
+                  <div className="courseOrgDisName">
+                    {courseDisName}
+                    {courseDisName.includes("National AI Olympiad") && "™"}
+                  </div>
+                )}
+              </div>
+            )}
         </header>
         <CollapseMenuBody isOpen={isOpen} />
       </>
