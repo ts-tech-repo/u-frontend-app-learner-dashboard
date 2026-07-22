259ea27669168be1e97613a0b584a8bf3b711730
Aman-K-Ray <aman.kumar.a.ray@accenture.com>
2026-07-22 14:34:47 +0530

    fix

--- src/containers/LearnerDashboardHeader/CollapsedHeader/CollapseMenuBody.jsx
+++ src/containers/LearnerDashboardHeader/CollapsedHeader/CollapseMenuBody.jsx
@@ -4,23 +4,66 @@ import PropTypes from 'prop-types';
 import { getConfig } from '@edx/frontend-platform';
 import { useIntl } from '@edx/frontend-platform/i18n';
 import { AppContext } from '@edx/frontend-platform/react';
-import { Button } from 'ts-paragon';
+import { Button, Badge } from 'ts-paragon';
+import { useMasqueradeBarData } from '../../MasqueradeBar/hooks';
 
 import WidgetNavbar from 'containers/WidgetContainers/WidgetNavbar';
+import urls from 'data/services/lms/urls';
+import { reduxHooks } from 'hooks';
 import { COLLAPSED_NAVBAR } from 'widgets/RecommendationsPaintedDoorBtn/constants';
+
+import { findCoursesNavDropdownClicked } from '../hooks';
 import messages from '../messages';
+import { useActiveTab } from '../../../ActiveTabContext';
 
 export const CollapseMenuBody = ({ isOpen }) => {
   const { formatMessage } = useIntl();
+  const siteNameMessage = formatMessage(messages['with.site.name'], { siteName: getConfig().SITE_NAME });
+  
   const { authenticatedUser } = React.useContext(AppContext);
+  const {
+    canMasquerade,
+  } = useMasqueradeBarData({ authenticatedUser });
+
+  const dashboard = reduxHooks.useEnterpriseDashboardData();
+  const { courseSearchUrl } = reduxHooks.usePlatformSettingsData();
+
+  const exploreCoursesClick = findCoursesNavDropdownClicked(urls.baseAppUrl(courseSearchUrl));
+
+  // Get groupedCourses and tabNames
+  const groupedCourses = reduxHooks.useGroupedCoursesData();
+  const tabNames = reduxHooks.useOrderedCoursesLabel() || [];
+  const { activeTab } = useActiveTab();
+
+  // Get the current tab's courseProvider name
+  let courseOrgDisName = '';
+  if (groupedCourses && tabNames.length > 0) {
+    const tabKey = tabNames[activeTab];
+    const tabObj = groupedCourses && groupedCourses[tabKey];
+    const tabCourses = tabObj && Array.isArray(tabObj.courses) ? tabObj.courses.flat() : [];
+    if (tabCourses.length > 0 && tabCourses[0].courseProvider) {
+      courseOrgDisName = tabCourses[0].courseProvider.name;
+    }
+  }
 
   return (
     isOpen && (
       <div className="d-flex flex-column shadow-sm nav-small-menu">
+        {courseOrgDisName && (
+          <Button as="a" variant="inverse-primary" className="courseOrgDisName">
+              {courseOrgDisName}
+            {courseOrgDisName.includes("National AI Olympiad") && "™"}
+          </Button>
+        )}
+        
         <WidgetNavbar placement={COLLAPSED_NAVBAR} />
-
         {authenticatedUser && (
           <>
+            {!!dashboard && (
+              <Button as="a" href={dashboard.url} variant="inverse-primary">
+                {formatMessage(messages.dashboard)}
+              </Button>
+            )}
             <Button
               as="a"
               href={`${getConfig().LMS_BASE_URL}/account/`}
@@ -28,7 +71,6 @@ export const CollapseMenuBody = ({ isOpen }) => {
             >
               {formatMessage(messages.account)}
             </Button>
-
             <Button
               as="a"
               href={getConfig().LOGOUT_URL}
--- src/containers/LearnerDashboardHeader/ExpandedHeader/AuthenticatedUserDropdown.jsx
+++ src/containers/LearnerDashboardHeader/ExpandedHeader/AuthenticatedUserDropdown.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 
 import { getConfig } from "@edx/frontend-platform";
+import { useIntl } from "@edx/frontend-platform/i18n";
 import { AppContext } from "@edx/frontend-platform/react";
-import { Dropdown, AvatarButton } from "ts-paragon";
+import { AvatarButton, Dropdown, Badge } from "ts-paragon";
+
+import messages from "../messages";
 
 export const AuthenticatedUserDropdown = () => {
   const { authenticatedUser } = React.useContext(AppContext);
+  const { formatMessage } = useIntl();
 
   return (
     <>
@@ -22,16 +26,12 @@ export const AuthenticatedUserDropdown = () => {
               {authenticatedUser.username}
             </span>
           </Dropdown.Toggle>
-
           <Dropdown.Menu className="dropdown-menu-right">
             <Dropdown.Item href={getConfig().ACCOUNT_SETTINGS_URL}>
-              Account
+              {formatMessage(messages.account)}
             </Dropdown.Item>
-
-            <Dropdown.Divider />
-
             <Dropdown.Item href={getConfig().LOGOUT_URL}>
-              Sign Out
+              {formatMessage(messages.signOut)}
             </Dropdown.Item>
           </Dropdown.Menu>
         </Dropdown>
